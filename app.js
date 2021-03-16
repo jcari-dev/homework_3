6f5e12b17aaf396945cff07ddb413860b1bcb286
jcari-rgb <582119jc@gmail.com>
2021-03-16 14:01:24 -0400

    landscaper: user can buy push lawnmower

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 let money = 0;
 let scissorsCost = 5;
+let old_timey_lawnmowerCost = 25;
 const tools = {
     teeth: 0.0416,
-    rustyScissors: 0.208
+    rustyScissors: 0.2083,
+    old_timey_lawnmower: 1.0416
 }
 
 function buyS() {
@@ -11,9 +13,26 @@ function buyS() {
         money -= scissorsCost;
         document.getElementById('profit').innerHTML = `Total Money: $${money}`;
         confirm(`You have ${money} left.`)
-        document.getElementById('sciBtn').style.visibility = "hidden";
+        document.getElementById('sciBtn').style.visibility = "hidden"; // I have 
+ //    tried so many things to try to properly disable that button, sadly this has been the best result
         document.getElementById('workBtn1').style.visibility = "hidden";
         document.getElementById('workBtn2').disabled = false;
+    } else {
+        confirm('Come back whenever!');
+    }
+}
+
+function buyOld() {
+    let buyOl = confirm(`Are you use you want to buy Old-timey Lawnmower, for $25? You have $${money} left.`);
+    if (buyOl == true) {
+        money -= old_timey_lawnmowerCost;
+        document.getElementById('profit').innerHTML = `Total Money: $${money}`;
+        confirm(`You have ${money} left.`)
+        document.getElementById('sciBtn2').style.visibility = "hidden"; // I have 
+ //    tried so many things to try to properly disable that button, sadly this has been the best result
+        document.getElementById('workBtn2').style.visibility = "hidden";
+        document.getElementById('workBtn2').disabled = true;
+        document.getElementById('workBtn3').disabled = false;
     } else {
         confirm('Come back whenever!');
     }
@@ -29,6 +48,15 @@ function disableBtn() {
     }
 }
 
+
+function disableBtn2() {
+    if (money >= 25) {
+        document.getElementById('sciBtn2').disabled = false;
+    } else {
+        document.getElementById('sciBtn2').disabled = true;
+    }
+}
+
 function work() {
     money += Math.round(tools.teeth * 24);
     console.log(money);
@@ -40,6 +68,17 @@ function work() {
 
 function work2() {
     money += Math.round(tools.rustyScissors * 24);
+    console.log(money);
+    document.getElementById('profit').innerHTML = `Total Money: $${money}`;
+    disableBtn2();
+
+
+}
+
+
+
+function work3() {
+    money += Math.round(tools.old_timey_lawnmower * 24);
     console.log(money);
     document.getElementById('profit').innerHTML = `Total Money: $${money}`;
     disableBtn();

7128e9382ad5fd7e4524373525c91839227252d8
jcari-rgb <582119jc@gmail.com>
2021-03-16 14:58:52 -0400

    landscaper: can use a team to cut grass

--- app.js
+++ app.js
@@ -7,18 +7,19 @@ const tools = {
     teeth: 0.0416, // 1 a day
     rustyScissors: 0.2083, // 5 a day
     old_timey_lawnmower: 2.0833, // 50 a day
-    fancy_BP_Lawnmower: 4.1666 // 100 a day
+    fancy_BP_Lawnmower: 4.1666, // 100 a day
+    jorge_and_friends: 10.4165 // 250 a day
 }
 
 // functions to buy new items/way to cut more grass
 
 
 function buyS() {
-    let buySc = confirm(`Are you sure you want to buy a pair scissors, for $5? You have $${money} left.`);
+    let buySc = confirm(`Yikes, that dentist bill is gonna be high. Do you want to buy a pair of rusty scissors for $5 dollars? You have $${money} dollars total.`);
     if (buySc == true) {
         money -= scissorsCost;
         document.getElementById('profit').innerHTML = `Total Money: $${money}`;
-        confirm(`You have ${money} left.`)
+        confirm(`You have ${money} dollars left.`)
         document.getElementById('sciBtn').style.visibility = "hidden"; // I have 
         //    tried so many things to try to properly disable that button, sadly this has been the best result
         document.getElementById('workBtn1').style.visibility = "hidden";
@@ -29,11 +30,11 @@ function buyS() {
 }
 
 function buyOld() {
-    let buyOl = confirm(`Are you sure you want to buy an Old-timey Lawnmower, for $25? You have $${money} left.`);
+    let buyOl = confirm(`Are you sure you want to buy an Old-timey Lawnmower for $25 dollars? You have $${money} dollars total.`);
     if (buyOl == true) {
         money -= old_timey_lawnmowerCost;
         document.getElementById('profit').innerHTML = `Total Money: $${money}`;
-        confirm(`You have ${money} left.`)
+        confirm(`You have ${money} dollars left.`)
         document.getElementById('sciBtn2').style.visibility = "hidden"; // I have 
         //    tried so many things to try to properly disable that button, sadly this has been the best result
         document.getElementById('workBtn2').style.visibility = "hidden";
@@ -45,11 +46,11 @@ function buyOld() {
 }
 
 function fancyBa() {
-    let buyBp = confirm(`Are you sure you want to buy a Fancy Battery-Powered Lawnmower, for $250? You have $${money} left.`);
+    let buyBp = confirm(`Are you sure you want to buy a Fancy Battery-Powered Lawnmower for $250 dollars? You have $${money} dollars total.`);
     if (buyBp == true) {
         money -= fancy_BP_LawnmowerCost;
         document.getElementById('profit').innerHTML = `Total Money: $${money}`;
-        confirm(`You have ${money} left.`)
+        confirm(`You have ${money} dollars left.`)
         document.getElementById('sciBtn3').style.visibility = "hidden"; // I have 
         //    tried so many things to try to properly disable that button, sadly this has been the best result
         document.getElementById('workBtn3').style.visibility = "hidden";
@@ -61,16 +62,16 @@ function fancyBa() {
 }
 
 function hireJorgeNFriends() {
-    let buyJorge = confirm(`Are you sure you want to hire a Jorge and his friends for $500? You have $${money} left.`);
+    let buyJorge = confirm(`Are you sure you want to hire a Jorge and his friends for $500 dollars? You have $${money} dollars total.`);
     if (buyJorge == true) {
         money -= jorge_and_friendsCost;
         document.getElementById('profit').innerHTML = `Total Money: $${money}`;
-        confirm(`You have ${money} left.`)
+        confirm(`You have ${money} dollars left, Jorge is very hungry ):`)
         document.getElementById('sciBtn4').style.visibility = "hidden"; // I have 
         //    tried so many things to try to properly disable that button, sadly this has been the best result
-        document.getElementById('workBtn3').style.visibility = "hidden";
+        document.getElementById('workBtn4').style.visibility = "hidden";
         document.getElementById('workBtn2').disabled = true;
-        document.getElementById('workBtn4').disabled = false;
+        document.getElementById('workBtn5').disabled = false;
     } else {
         confirm('Come back whenever!');
     }
@@ -145,6 +146,14 @@ function work3() {
 
 function work4() {
     money += Math.round(tools.fancy_BP_Lawnmower * 24);
+    console.log(money);
+    document.getElementById('profit').innerHTML = `Total Money: $${money}`;
+    disableBtn4();
+
+}
+
+function work5() {
+    money += Math.round(tools.jorge_and_friends * 24);
     console.log(money);
     document.getElementById('profit').innerHTML = `Total Money: $${money}`;
     disableBtn4();

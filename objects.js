c0263153029b539816593d214e3e7a4c513e09cc
jcari-rgb <582119jc@gmail.com>
2021-03-16 16:35:56 -0400

    created person class

--- objects.js
+++ objects.js
@@ -19,3 +19,54 @@ getPrice(){
 
 }
 
+class Person {
+  constructor(name){
+    this.name = name;
+  }
+  age = 0;
+  height = 0;
+  weight = 0;
+  mood = 0;
+  hamsters = [];
+  bankAccount = 0; //sad
+
+  getName(){
+    return this.name
+  }
+
+  getAge(){
+    return age
+  }
+
+  getWeight(){
+    return weight
+  }
+
+  greet(){
+    console.log('hi ' + Person.name)
+  }
+
+  eat(){
+    weight + 1,
+    mood + 1
+  }
+
+  exercise(){
+    weight - 1
+  }
+
+  ageUp(){
+    age + 1,
+    height + 1,
+    weight + 1,
+    mood -1,
+    bankAccount + 10
+  }
+
+  buyHamster(hamster){
+    hamster.push(Person.hamster),
+    mood +10,
+    backAccount -getPrice(Hamster)
+  }
+
+}
